6c731c791076d73dd9e83766a94e3a05bcf34e7e
chandan <chandan7979ck@gmail.com>
2024-03-01 22:40:00 +0530

    scrolltop and movie detail

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import ScrollToTop from './components/ScrollToTop';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+      <ScrollToTop />
       <App />
     </Router>
   </React.StrictMode>
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -1,7 +1,79 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { FaStar } from "react-icons/fa6";
+// import { MovieTitle } from "../hooks/MovieTitle.js";
+
 
 export const MovieDetail = () => {
+
+  const param = useParams();
+  const [movie, setmovie] = useState({});
+
+  // MovieTitle(movie.title);
+  const image = movie.poster_path ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` : "https://via.placeholder.com/500x750";
+  
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(`https://api.themoviedb.org/3/movie/${param.id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US`);
+      const json = await response.json();
+      setmovie(json);
+      console.log(json);
+    }
+    fetchData();
+  }, []);
+
   return (
-    <main>MovieDetail</main>
+    <main>
+      <section className="flex justify-around flex-wrap py-5">
+        <div className="max-w-sm">
+          <img className="rounded" src={image} alt={movie.title} />
+        </div>
+
+        <div className="max-w-2xl text-gray-700 text-lg dark:text-white">
+          <h1 className="text-4xl font-bold my-3 text-center lg:text-left">{movie.title}</h1>
+          <p className="my-4">{movie.overview}</p>
+            { movie.genres ? (
+              <p className="my-7 flex flex-wrap gap-2">
+              { movie.genres.map((genre) => (
+                <span className="mr-2 border border-gray-200 rounded dark:border-gray-600 p-2" key={genre.id}>{genre.name}</span>
+              )) }
+            </p>
+            ) : "" }
+          
+          <div className="flex items-center">
+              < FaStar className="w-5 h-5 text-yellow-400" />
+              <p className="ml-2 text-gray-900 dark:text-white">{movie.vote_average}</p>
+              <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
+              <span className="text-gray-900 dark:text-white">{movie.vote_count} reviews</span>
+          </div>
+
+          <p className="my-4">
+            <span className="mr-2 font-bold">Runtime:</span>
+            <span>{movie.runtime} min.</span>
+          </p>
+
+          <p className="my-4">
+            <span className="mr-2 font-bold">Budget:</span>
+            <span>{movie.budget}</span>
+          </p>
+
+          <p className="my-4">
+            <span className="mr-2 font-bold">Revenue:</span>
+            <span>{movie.revenue}</span>
+          </p>
+
+          <p className="my-4">
+            <span className="mr-2 font-bold">Release Date:</span>
+            <span>{movie.release_date}</span>
+          </p>
+
+          <p className="my-4">
+            <span className="mr-2 font-bold">IMDB Code:</span>
+            <a href={`https://www.imdb.com/title/${movie.imdb_id}`} target="_blank" rel="noreferrer">{movie.imdb_id}</a>
+          </p>
+        </div>
+      </section>
+    </main>
   )
 }
